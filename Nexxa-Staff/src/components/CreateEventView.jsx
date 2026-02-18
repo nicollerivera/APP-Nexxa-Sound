21f4c48a0cbc3df6b79bb3820b6db3c384991901
nicollerivera <93458431+nicollerivera@users.noreply.github.com>
2026-02-17 22:53:14 -0500

    Fix: Añadir import useEffect faltante en CreateEventView.jsx (resolviendo crash de página)

--- Nexxa-Staff/src/components/CreateEventView.jsx
+++ Nexxa-Staff/src/components/CreateEventView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TimeInput from './TimeInput';
 import { PRICING } from '../utils/constants';
 import {
